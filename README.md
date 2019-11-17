6ef9029d5b59822f51310b435d5faf6ed8f4844b
Yann Szymanski <44467955+yannsmni@users.noreply.github.com>
2019-11-18 00:07:20 +0100

    Adding README.md and the sql file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # API-Node.JS
-This is the API of Node.JS
+# Requirements :
+
+This API run under Node.js. It require also a custom database file named "users.sql". In addition to that, you have to install body parser, mysql and express. You can install these packages with the commands ` npm install express`, ` npm install body-parser` and ` npm install mysql`.
+
+# Run :
+
+To run this api, you have to use the command ` node index.js`.
+
---
+++ users.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 17 nov. 2019 à 22:56
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `prenom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `localisation` varchar(255) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `mot_de_passe` varchar(255) COLLATE utf8_bin NOT NULL,
+  `date_creation` datetime NOT NULL,
+  `role` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
